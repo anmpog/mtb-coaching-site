021a7a068436fe35787e1aaddafc46f87827f5e7
Anthony Pogliano <poglianoster@gmail.com>
2026-02-02 12:42:29 -0700

    chore: downgrading tailwind, config
    Downgrading tailwind for more reliable compatibility with older iOS
    versions.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'astro/config'
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-})
+  integrations: [tailwind(), icon()],
+});
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+};
