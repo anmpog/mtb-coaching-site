7b698ec62c95c3823421b51a47d9eacbf153f2e6
Anthony Pogliano <poglianoster@gmail.com>
2026-02-05 15:40:52 -0700

    wip(responsive-nav): TS refinements
    Adding some explicit types to script that controls nav menu in mobile.

--- src/scripts/nav-menu.ts
+++ src/scripts/nav-menu.ts
@@ -1,11 +1,15 @@
-const navMenuButton = document.getElementById('menu-button')
-const navMenuBackdrop = document.getElementById('menu-backdrop')
-const navMenuWrapper = document.getElementById('menu-wrapper')
+const navMenuButton = document.getElementById(
+  'menu-button',
+) as HTMLButtonElement
+const navMenuBackdrop = document.getElementById(
+  'menu-backdrop',
+) as HTMLDivElement
+const navMenuWrapper = document.getElementById('menu-wrapper') as HTMLDivElement
 
 function openMenu() {
-  navMenuButton?.setAttribute('aria-expanded', 'true')
-  navMenuWrapper?.classList.add('expanded')
-  navMenuBackdrop?.classList.add('blur')
+  navMenuButton.setAttribute('aria-expanded', 'true')
+  navMenuWrapper.classList.add('expanded')
+  navMenuBackdrop.classList.add('blur')
   document.body.classList.add('block-scroll')
 
   window.addEventListener('keydown', detectEscKeyPress)
@@ -13,20 +17,18 @@ function openMenu() {
 }
 
 function closeMenu() {
-  navMenuButton?.setAttribute('aria-expanded', 'false')
-  navMenuWrapper?.classList.remove('expanded')
-  navMenuBackdrop?.classList.remove('blur')
+  navMenuButton.setAttribute('aria-expanded', 'false')
+  navMenuWrapper.classList.remove('expanded')
+  navMenuBackdrop.classList.remove('blur')
   document.body.classList.remove('block-scroll')
 
   window.removeEventListener('keydown', detectEscKeyPress)
   window.removeEventListener('click', detectClickOutsideMenu)
 }
 
-function detectClickOutsideMenu(event) {
-  if (
-    !navMenuWrapper?.contains(event.target) &&
-    !navMenuButton?.contains(event.target)
-  ) {
+function detectClickOutsideMenu(event: MouseEvent) {
+  const target = event.target as Node
+  if (!navMenuWrapper.contains(target) && !navMenuButton.contains(target)) {
     closeMenu()
   }
 }
@@ -37,11 +39,11 @@ function detectEscKeyPress(event: KeyboardEvent) {
   }
 }
 
-navMenuButton?.addEventListener('click', (event) => {
-  event?.stopPropagation()
-  if (navMenuButton?.getAttribute('aria-expanded') === 'false') {
+navMenuButton.addEventListener('click', (event) => {
+  event.stopPropagation()
+  if (navMenuButton.getAttribute('aria-expanded') === 'false') {
     openMenu()
-  } else if (navMenuButton?.getAttribute('aria-expanded') === 'true') {
+  } else if (navMenuButton.getAttribute('aria-expanded') === 'true') {
     closeMenu()
   }
 })
