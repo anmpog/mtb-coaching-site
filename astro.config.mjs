7f21c0956dd0ef4abebb50512ee32713ef09c032
Anthony Pogliano <poglianoster@gmail.com>
2026-02-25 17:11:03 -0700

    chore(eslint): update/correct linting config
    Noticed that some linting rules were not being applied as expected, so
    I took a brief detour to correct these issues.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
-import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
-import { defineConfig } from "astro/config";
+import tailwind from '@astrojs/tailwind'
+import icon from 'astro-icon'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-});
+})
--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,24 @@
-import eslintPluginAstro from 'eslint-plugin-astro'
-import jsxA11y from 'eslint-plugin-jsx-a11y'
+// import jsxA11y from 'eslint-plugin-jsx-a11y'
+import astro from 'eslint-plugin-astro'
 import { defineConfig } from 'eslint/config'
 import tseslint from 'typescript-eslint'
 
 export default defineConfig([
+  { ignores: ['.astro/**', 'dist/**', '**/*.d.ts'] },
   ...tseslint.configs.recommended,
-  ...eslintPluginAstro.configs.recommended,
-  {
-    files: ['**/*.astro'],
-    plugins: {
-      'jsx-a11y': jsxA11y,
-    },
-    rules: { ...jsxA11y.configs.recommended.rules },
-  },
+  astro.configs.recommended,
+  astro.configs['jsx-a11y-recommended'],
   {
     files: ['**/*.{astro,ts,tsx,js,mjs}'],
     rules: {
       'no-console': 'error',
+    },
+  },
+  {
+    files: ['**/*.astro'],
+    rules: {
+      'astro/sort-attributes': 'error',
+      'astro/no-set-html-directive': 'error',
     },
   },
 ])
