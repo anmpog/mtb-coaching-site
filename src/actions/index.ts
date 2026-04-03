568d8fbd02005089b0baad98573a742a49915967
Anthony Pogliano <poglianoster@gmail.com>
2026-04-02 18:18:30 -0600

    wip: refinements to validation, styling, submit
    - More detailed validation messaging and logic, with some regex applied
    to try and ensure a human readable submission when submission
    originates on the client
    - Styling tweaks related to submission state wherein form submit button
    receives styling changes + displays an icon when a submission is
    successfully validated
    - Submission script expanded to update DOM elements as appropriate

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -5,7 +5,7 @@ export const server = {
   signUpForm: defineAction({
     accept: 'form',
     input: z.object({
-      'newsletter-email': z.string().email().max(254),
+      'newsletter-email': z.string().trim().email().max(254),
       'newsletter-confirm': z.string().max(254).optional(),
     }),
     handler: async () => {
@@ -16,11 +16,33 @@ export const server = {
   contactUsForm: defineAction({
     accept: 'form',
     input: z.object({
-      name: z.string().min(2).max(100),
-      'contact-email': z.string().email().max(254),
+      name: z
+        .string()
+        .trim()
+        .min(2, { message: 'Name must be longer than 2 characters' })
+        .max(100, { message: 'Name cannot exceed 100 characters' })
+        .regex(/^[a-zA-Z\s'\-]+$/, {
+          message:
+            'Name should only contain letters, spaces, hyphens, and apostrophes',
+        }),
+      'contact-email': z.string().trim().email().max(254),
       'contact-confirm': z.string().max(254).optional(),
-      subject: z.string().min(3).max(150),
-      message: z.string().min(10).max(1000),
+      subject: z
+        .string()
+        .trim()
+        .min(3, { message: 'Subject must be longer than 3 characters' })
+        .max(150, { message: 'Subject cannot exceed 150 characters' })
+        .regex(/^[^<>{}()]*$/, {
+          message: 'Subject can only contain valid characters',
+        }),
+      message: z
+        .string()
+        .trim()
+        .min(10)
+        .max(1000)
+        .regex(/^[^<>{}()]*$/, {
+          message: 'Message can only contain valid characters',
+        }),
     }),
     handler: async () => {
       // Action is just adding server-side validation, no other logic needed
--- src/scripts/form-submit.ts
+++ src/scripts/form-submit.ts
@@ -1,16 +1,20 @@
-import { actions, isInputError } from 'astro:actions'
+import { actions, isActionError, isInputError } from 'astro:actions'
 
 export default function initFormHandler(form: HTMLFormElement) {
-  const formAction = form.dataset.action
+  // Get form fields in array structure so it's easier iterate over them
   const formFieldsArr = Array.from(
     form.querySelectorAll('.form-control:not([data-optional])'),
   )
 
+  // Map structure so that iteration isn't necessary for updating individual
+  // fields
   const formFieldMap = new Map(
     formFieldsArr.map((formField) => {
       const inputElem = formField.querySelector('input, textarea') as
         | HTMLInputElement
         | HTMLTextAreaElement
+
+      // Error field id attributes end with '-error' by convention
       const errorElem = formField.querySelector(
         'p[id$="-error"]',
       ) as HTMLParagraphElement
@@ -25,32 +29,60 @@ export default function initFormHandler(form: HTMLFormElement) {
     }),
   )
 
-  // Listener to reset server-side error visibility on input change
+  // Listener to reset server-side error visibility on input event
   formFieldMap.forEach((formField) => {
     const input = formField.inputElem
     const errorElem = formField.errorElem
 
     input.addEventListener('input', () => {
       errorElem.textContent = errorElem.dataset.defaultErrorMessage || ''
-      // errorElem.classList.remove('visible')
+      errorElem.classList.remove('visible')
     })
   })
-
-  // console.log('Form inputs: ', formInputs)
 
   // Form submission listener/handler with Astro action
   form.addEventListener('submit', async (event) => {
     event.preventDefault()
+    const formAction = form.dataset.action as keyof typeof actions
     const formData = new FormData(form)
-    const { error } = await actions[formAction](formData)
+    const { data, error } = await actions[formAction](formData)
 
     // Handle validation error from action/server
     if (isInputError(error)) {
       Object.entries(error.fields).forEach(([fieldName, messages]) => {
-        const errorElemToUpdate = formFieldMap.get(fieldName).errorElem
-        errorElemToUpdate.textContent = messages[0]
-        errorElemToUpdate?.classList.add('visible')
+        const errorElemToUpdate = formFieldMap.get(fieldName)
+        if (!errorElemToUpdate) return
+        errorElemToUpdate.errorElem.textContent = messages[0]
+        errorElemToUpdate.errorElem.classList.add('visible')
       })
+
+      // early exit
+      return
+    }
+
+    if (isActionError(error)) {
+      console.error(error.message, error.code)
+
+      // early exit
+      return
+    }
+
+    if (data) {
+      const submitButton = form.querySelector(
+        '[type=submit]',
+      ) as HTMLButtonElement
+      const submitButtonText = form.querySelector(
+        '[type="submit"] span',
+      ) as HTMLSpanElement
+      const submitButtonIcon = form.querySelector(
+        '[type="submit"] svg',
+      ) as HTMLElement
+
+      submitButtonText.innerHTML = 'Submitted'
+      submitButton.toggleAttribute('disabled')
+      submitButtonIcon.classList.add('visible')
+
+      return data
     }
   })
 }
