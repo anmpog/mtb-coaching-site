bb28bcfb3aa4a9c93d53ded065907b35ed1dbc81
Anthony Pogliano <poglianoster@gmail.com>
2026-06-18 13:45:13 -0600

    feature(fonts): fonts configured
    Addition of Rubik font, confirmed delivery to browser when font is used.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,36 @@
 import netlify from '@astrojs/netlify'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
   adapter: netlify({
-    imageCDN: false
+    imageCDN: false,
   }),
   output: 'static',
+  fonts: [
+    {
+      provider: fontProviders.npm({
+        remote: false,
+      }),
+      name: 'Rubik Variable',
+      cssVariable: '--font-rubik',
+      options: {
+        package: '@fontsource-variable/rubik',
+      },
+    },
+    {
+      provider: fontProviders.npm({
+        remote: false,
+      }),
+      name: 'Raleway Variable',
+      cssVariable: '--font-raleway',
+      options: {
+        package: '@fontsource-variable/raleway',
+      },
+      styles: ['normal'],
+    },
+  ],
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,15 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
   theme: {
     extend: {
-      fontFamily: {
-        heading: ['Raleway', 'sans-serif'],
-      },
       colors: {
         'active-link': 'rgb(49, 127, 129)',
         'brand-blue': '#57bbbf',
         'brand-red': '#C8102E',
       },
+    },
+    fontFamily: {
+      heading: ['var(--font-raleway)', 'sans-serif'],
+      body: ['var(--font-rubik)'],
     },
   },
 }
